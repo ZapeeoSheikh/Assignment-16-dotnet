943ac4e2b14d14f1b7bfc0f91faef0c759e5a902
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-02-26 23:34:58 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_16.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult OurTeam()
+        {
+            return View();
+        }
+    }
+}
